e082af0e2539823b20723fb7d56929e453f90f7e
write4alive <cenk.adalan@gmail.com>
2021-09-12 22:16:34 +0300

    Update data_pipeline_dag.py

--- airflow/dags/data_pipeline_dag.py
+++ airflow/dags/data_pipeline_dag.py
@@ -20,7 +20,7 @@ default_args = {
 dag = DAG('data_pipeline_dag',
           default_args = default_args,
           description = 'Load and transform data in Redshift with Airflow',
-          schedule_interval = '0 * * * *',
+          schedule_interval = '@hourly',
           max_active_runs = 1
         )
 
