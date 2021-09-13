41ba774f2f8a34c14f503bf6feb65681257f2aa4
write4alive <cenk.adalan@gmail.com>
2021-09-13 08:32:46 +0300

    Update stage_redshift.py

--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -40,6 +40,7 @@ class StageToRedshiftOperator(BaseOperator):
         
 
     def execute(self, context):
+        self.log.info('Getting credentials')
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
